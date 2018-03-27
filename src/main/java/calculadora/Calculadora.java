fe36fb3cb555eabe909605fc1032567dcafc9284
andreendo <andreendo@gmail.com>
2018-03-27 10:51:54 -0300

    first commit

---
+++ src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,43 @@
+package calculadora;
+
+/**
+ *
+ * @author andreendo
+ */
+class Calculadora {
+    int num1, num2;
+    int res;
+    String mensagem;
+    
+    void entrarNumero01(int num) {
+        num1 = num;
+    }
+
+    void entrarNumero02(int num) {
+        num2 = num;
+    }
+
+    void adicionar() {
+        res = num1 + num2;
+    }
+
+    int getResultado() {
+        return res;
+    }
+
+    void dividir() {
+        if(num2 == 0) {
+            res = -1;
+            mensagem = "erro: divisao por zero";
+        }
+        else
+            res = num1 / num2;
+        
+        
+    }
+
+    String getMensagem() {
+        return mensagem;
+    }
+    
+}
