66d03918f4518bab9c0c903b26c4938c487d1f63
Roberto <juninhoguimar@yahoo.com.br>
2018-03-28 20:56:19 -0300

    terceiro ciclo

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -1,12 +1,11 @@
 package calculadora;
 
-
-
 class Calculadora {
+
     int num1, num2;
     int res;
     String mensagem;
-    
+
     void entrarNumero01(int num) {
         num1 = num;
     }
@@ -18,28 +17,31 @@ class Calculadora {
     void adicionar() {
         res = num1 + num2;
     }
-    
-      void subtrair() {
+
+    void subtrair() {
         res = num1 - num2;
     }
-      
+
     int getResultado() {
         return res;
     }
 
+    void multiplicar() {
+        res = num1 * num2;
+    }
+
     void dividir() {
-        if(num2 == 0) {
+        if (num2 == 0) {
             res = -1;
             mensagem = "erro: divisao por zero";
-        }
-        else
+        } else {
             res = num1 / num2;
-        
-        
+        }
+
     }
 
     String getMensagem() {
         return mensagem;
     }
-    
+
 }
